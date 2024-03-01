418503b4ca9e2de5721c5535836d3a0223fcee29
philw <phil@pwood.net>
2024-03-01 20:02:40 +0000

    Basic motor control working

---
+++ src/config-bot-one.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+
+// hardware configuration
+
+//**** IO CONFIGURATION ****************************************************//
+const uint8_t ENCODER_LEFT_CLK = 2;
+const uint8_t ENCODER_RIGHT_CLK = 3;
+const uint8_t ENCODER_LEFT_B = 4;
+const uint8_t ENCODER_RIGHT_B = 5;
+const uint8_t USER_IO = 6;
+const uint8_t MOTOR_LEFT_DIR = 7;
+const uint8_t MOTOR_RIGHT_DIR = 8;
+const uint8_t MOTOR_LEFT_PWM = 9;
+const uint8_t MOTOR_RIGHT_PWM = 10;
+const uint8_t EMITTER_A = 11;
+const uint8_t EMITTER_B = 12;
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,56 @@
 #include <Arduino.h>
+#include "config-bot-one.h"
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // define and initialise motor pins
+    pinMode(MOTOR_LEFT_DIR, OUTPUT);
+    pinMode(MOTOR_RIGHT_DIR, OUTPUT);
+    pinMode(MOTOR_LEFT_PWM, OUTPUT);
+    pinMode(MOTOR_RIGHT_PWM, OUTPUT);
+    digitalWrite(MOTOR_LEFT_PWM, 0);
+    digitalWrite(MOTOR_LEFT_DIR, 0);
+    digitalWrite(MOTOR_RIGHT_PWM, 0);
+    digitalWrite(MOTOR_RIGHT_DIR, 0);
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
+
+  digitalWrite(MOTOR_RIGHT_DIR, HIGH);
+  analogWrite(MOTOR_RIGHT_PWM, 50);
+  digitalWrite(MOTOR_LEFT_DIR, HIGH);
+  analogWrite(MOTOR_LEFT_PWM, 50);
+
+  delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+  digitalWrite(MOTOR_RIGHT_DIR, LOW);
+  analogWrite(MOTOR_RIGHT_PWM, 0);
+  digitalWrite(MOTOR_LEFT_DIR, LOW);
+  analogWrite(MOTOR_LEFT_PWM, 0);
+
+  delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  digitalWrite(MOTOR_RIGHT_DIR, LOW);
+  analogWrite(MOTOR_RIGHT_PWM, 50);
+  digitalWrite(MOTOR_LEFT_DIR, LOW);
+  analogWrite(MOTOR_LEFT_PWM, 50);
+
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+  digitalWrite(MOTOR_RIGHT_DIR, HIGH);
+  analogWrite(MOTOR_RIGHT_PWM, 0);
+  digitalWrite(MOTOR_LEFT_DIR, HIGH);
+  analogWrite(MOTOR_LEFT_PWM, 0);
+
   delay(2000);                       // wait for a second
 }
 
